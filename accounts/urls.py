b82fc091180c67c555e9a8f88dad44ec4f62eb45
strawHatRobby <mailrobinfrancis@gmail.com>
2017-06-19 17:17:56 +0530

    "development branch"

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-
+from django.contrib.auth.views import (login,
+    logout, logout_then_login)
 from . import views
 
 urlpatterns = [
     url(r'^$',views.dashboard,name='dashboard'),
-    url(r"login/$", 'django.contrib.auth.views.login', name="login"),
-    url(r"logout/$", 'django.contrib.auth.views.logout', name="logout"),
-    url(r"logout-then-login/$",'django.contrib.auth.views.logout_then_login', name="logout_then_login"),
+    url(r"login/$", login, name="login"),
+    url(r"logout/$",logout, name="logout"),
+    url(r"logout-then-login/$",logout_then_login, name="logout_then_login"),
 ]
