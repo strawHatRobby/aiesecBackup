d8d61da696c209410e5c42d0dd7be4382ff806e8
strawHatRobby <mailrobinfrancis@gmail.com>
2017-06-21 00:53:06 +0530

    "got the default layouts working"

--- universal/urls.py
+++ universal/urls.py
@@ -4,4 +4,16 @@ from . import views
 
 urlpatterns = [
         url(r'^$',views.home,name='home'),
+        url(r'awdocuments$',views.awdocuments,name='awdocuments'),
+        url(r'contacts$',views.contacts,name='contacts'),
+        url(r'guidesigt$',views.guidesigt,name='guidesigt'),
+        url(r'guidesigv$',views.guidesigv,name='guidesigv'),
+        url(r'igt$',views.igt,name='igt'),
+        url(r'igv$',views.igt,name='igv'),
+        url(r'ldmresources$',views.ldmresources,name='ldmresources'),
+        url(r'notices$',views.notices,name='notices'), #convert to notifications later
+        url(r'opportunities$',views.opportunities,name='opportunities'),
+        url(r'projects$',views.projects,name='projects'),
+        url(r'visa_for_externals$',views.visa_for_externals,name='visa_for_externals'),
+
 ]
--- universal/views.py
+++ universal/views.py
@@ -3,3 +3,36 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'universal/home.html')
+
+def contacts(request):
+    return render(request, 'universal/contacts.html')
+
+def awdocuments(request):
+    return render(request, 'universal/awdocuments.html')
+
+def guidesigt(request):
+    return render(request, 'universal/guidesigt.html')
+
+def guidesigv(request):
+    return render(request, 'universal/home.html')
+
+def igt(request):
+    return render(request, 'universal/igt.html')
+
+def igv(request):
+    return render(request, 'universal/igv.html')
+
+def ldmresources(request):
+    return render(request, 'universal/ldmresources.html')
+
+def notices(request):
+    return render(request, 'universal/notices.html')
+
+def opportunities(request):
+    return render(request, 'universal/opportunities.html')
+
+def projects(request):
+    return render(request, 'universal/projects.html')
+
+def visa_for_externals(request):
+    return render(request, 'universal/visaforexternals.html')
